196806be1238410d8f414a42e32f7ce4a0612f2f
Michael Musick <michael@michaelmusick.com>
2017-10-23 10:55:12 -0600

    added basic structure an box2d Lib

---
+++ box.js
@@ -0,0 +1,6 @@
+
+// A rectangular box
+
+function Box(x, y ) {
+
+}
---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+
+// A reference to our box2d world
+var world;
+
+// A list for all of our boxes
+var boxes = [];
+
+function setup() {
+  createCanvas(640,360);
+
+  // Initialize box2d physics and create the world
+  world = createWorld(new box2d.b2Vec2(0,0));
+  world.SetGravity(new box2d.b2Vec2(0,20));
+
+}
+
+function draw() {
+  background(51);
+
+  // We must always step through time!
+  var timeStep = 1.0/frameRate();
+  // 2nd and 3rd arguments are velocity and position iterations
+  // Position Iteration, adjusts how much, each object can reposition itself, per timestep.
+  world.Step(timeStep,10,10);
+
+
+  // Display all the boxes
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].display();
+  }
+}
