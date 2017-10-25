10c7732acb7b2ef3797ebb5208354bab8b7afb02
StephenRound <sixty3zero@yahoo.com>
2017-10-25 11:40:16 -0600

    adding limb class
    has some goals in comments. no code yet

---
+++ limb.js
@@ -0,0 +1,16 @@
+//starting out with constructing a limb class consisting of two bodies joined
+
+//subproblems
+  //make the boxes correctly
+  //set the correct anchor points
+  //make sure they can't bend into themselves
+
+  //extra goals:
+    //add feet/hands
+    //define an arm and leg parameter and limit rotation accordingly
+
+function Limb(x, y) {
+  
+
+
+}
