425292529791fdb776cebe7be01bd486c530e436
StephenRound <sixty3zero@yahoo.com>
2017-10-25 12:19:20 -0600

    added some comments
    if you add comments, leave initials too

--- limb.js
+++ limb.js
@@ -9,13 +9,25 @@
     //add feet/hands
     //define an arm and leg parameter and limit rotation accordingly
 
+//if you leave a comment leave your initials too -sr
+
+
 function Limb(x, y) {
-  this.box1 = new Box(x, y, 5, 10, false);
-  this.box2 = new Box(x, y, 5, 10, false);
+  this.box1 = new Box(x, y, 50, 130, false);
+  this.box2 = new Box(x, y, 50, 130, false); //the box class has a final parameter
+  //that'll set it to a static (true) or dynamic (false) body. mostly for testing
+  //purposes. we won't need to make the ragdoll static -sr
 
   var rjd = new box2d.b2RevoluteJointDef();
 
   rjd.Initialize(this.box1.body, this.box2.body, this.box1.body.GetWorldCenter());
+  rjd.lowerAngle = -0.5 * TWO_PI;
+  rjd.upperAngle = 0.25 * TWO_PI; //!!! revolute joints have angle limits built in
+  //and while i realize i'm trying to accomplish something that we don't even need
+  //to accomplish, this is a good discovery nonetheless. -sr
+  rjd.enableLimit = true;
+
+
 
   joint = world.CreateJoint(rjd);
 
@@ -26,6 +38,9 @@ function Limb(x, y) {
     var anchor = scaleToPixels(this.box1.body.GetWorldCenter());
     fill(0);
     noStroke();
-    ellipse(anchor.x, anchor.y, 4, 4); //a temporary elbow
+    ellipse(anchor.x, anchor.y, 4, 4); //a temporary elbow  -sr
   };
+
+  //this code is more or less ripped straight from the revolute joint example
+  //it feels like it's the correct joint to use, but i could just be wrong -sr
 }
--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup() {
     world.SetGravity(new box2d.b2Vec2(0, 10));
 
     for (var i = 0; i < 2; i++) {
-      boxes[i] = new Limb(width/2, height/2);
+      boxes[i] = new Limb(100, 100);
     }
 
 }
