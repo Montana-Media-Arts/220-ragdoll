a9706f0aa06f4801317ac1c7d7915f9e93a88256
StephenRound <sixty3zero@yahoo.com>
2017-11-08 10:17:11 -0700

    commented out images until ragdoll is ready

--- limb.js
+++ limb.js
@@ -10,30 +10,29 @@
     //define an arm and leg parameter and limit rotation accordingly
 
 
-var head, rleg, body, lleg, rarm, larm;
-
-//if you leave a comment leave your initials too -sr
-function preload(){
-head= loadImage("bodyImg/coralinehead.png");
-rleg = loadImage("bodyImg/coralineRleg.png")
-body= loadImage("bodyImg/coralinebody.png");
-lleg = loadImage("bodyImg/coralineLleg.png");
-rarm= loadImage("bodyImg/coralineRarm.png");
-larm = loadImage("bodyImg/coralineLarm.png");
-
-}
+// var head, rleg, body, lleg, rarm, larm;
+//
+// //if you leave a comment leave your initials too -sr
+// function preload(){
+//   head= loadImage("bodyImg/coralinehead.png");
+//   rleg = loadImage("bodyImg/coralineRleg.png")
+//   body= loadImage("bodyImg/coralinebody.png");
+//   lleg = loadImage("bodyImg/coralineLleg.png");
+//   rarm= loadImage("bodyImg/coralineRarm.png");
+//   larm = loadImage("bodyImg/coralineLarm.png");
+//
+// }
 
 function Limb(x, y) {
   this.box1 = new Box(x, y, 20, 60);
-  this.box2 = new Box(x, y, 20, 70); //the box class has a final parameter
-  //that'll set it to a static (true) or dynamic (false) body. mostly for testing
-  //purposes. we won't need to make the ragdoll static -sr
+  this.box2 = new Box(x, y, 20, 70);
 
   var rjd = new box2d.b2RevoluteJointDef();
 
   rjd.Initialize(this.box1.body, this.box2.body, this.box1.body.GetWorldCenter());
   rjd.lowerAngle = -0.5 * TWO_PI;
-  rjd.upperAngle = 0.25 * TWO_PI; //!!! revolute joints have angle limits built in
+  rjd.upperAngle = 0.25 * TWO_PI;
+  //!!! revolute joints have angle limits built in
   //and while i realize i'm trying to accomplish something that we don't even need
   //to accomplish, this is a good discovery nonetheless. -sr
   rjd.enableLimit = true;
@@ -59,6 +58,4 @@ function Limb(x, y) {
     ellipse(anchor.x, anchor.y, 4, 4); //a temporary elbow  -sr
   };
 
-  //this code is more or less ripped straight from the revolute joint example
-  //it feels like it's the correct joint to use, but i could just be wrong -sr
 }
