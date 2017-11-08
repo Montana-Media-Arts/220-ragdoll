02229c187765481d179256cb7ca5ef6f4bb4b77c
Michael Musick <michael@michaelmusick.com>
2017-11-08 10:30:21 -0700

    both arms

--- Body.js
+++ Body.js
@@ -2,12 +2,15 @@ function Body(x, y) {
 
      this.torso = new Box(x, y - 30, 120, 10, false);
      this.leftArm = new Box(x, y, 10, 40, false);
-     this.rightArm = new Box(x, y, 10, 40, false);
+     this.rightArm = new Box(x, y, 10, 60, false);
 
      // Define joint as between two bodies
      var rjd = new box2d.b2RevoluteJointDef();
 
      rjd.Initialize(this.torso.body, this.leftArm.body, this.torso.body.GetWorldCenter());
+     joint = world.CreateJoint(rjd);
+
+     var rjd = new box2d.b2RevoluteJointDef();
      rjd.Initialize(this.torso.body, this.rightArm.body, this.torso.body.GetWorldCenter());
 
      joint = world.CreateJoint(rjd);
