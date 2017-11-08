78579561ed17342b29e7426a64c918a3cee4aea6
Michael Musick <michael@michaelmusick.com>
2017-11-08 10:18:50 -0700

    consolidate

--- sketch.js
+++ sketch.js
@@ -92,3 +92,12 @@ function mousePressed() {
         spring.bind(worldPos.mouseX, worldPos.mouseY, box);
     }
 }
+
+
+function findCenter( centerObj ){
+
+    let x = centerObj.pos.x - ( width/2);
+    let y = centerObj.pos.y - ( height/2);
+
+    return { x: x, y: y };
+}
