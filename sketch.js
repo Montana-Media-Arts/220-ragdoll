3f0ed1336afc92c4782298ee0836162ead2f6c0f
Michael Musick <michael@michaelmusick.com>
2017-11-08 09:42:03 -0700

    basic moving camera

--- sketch.js
+++ sketch.js
@@ -7,7 +7,10 @@ var boundaries = [];
 //var bg;
 var spring;
 
-let worldPos = { x: 0, y:0 };
+let worldPos = {
+    x: 0,
+    y: 0
+};
 
 //function preload() {
 //  bg = loadImage("backgrounds/skyone.jpg");
@@ -15,62 +18,73 @@ let worldPos = { x: 0, y:0 };
 
 function setup() {
 
-     createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 
 
-     // Initialize box2d physics and create the world
-     world = createWorld();
-     //world.SetGravity(new box2d.b2Vec2(0, 10)); < this didn't even do anything
+    // Initialize box2d physics and create the world
+    world = createWorld();
+    //world.SetGravity(new box2d.b2Vec2(0, 10)); < this didn't even do anything
 
-     //limb = new Limb (width / 2, height / 2);
+    //limb = new Limb (width / 2, height / 2);
 
-     spring = new Spring();
+    spring = new Spring();
 
-     box = new Box(width / 2, height / 2, 30, 30);
+    box = new Box(width / 2, height / 2, 30, 30);
 
-     boundaries.push(new Boundary(width / 2, height / 2 + 100, 100, 10));
-     boundaries.push(new Boundary(3 * width / 4, height - 50, width / 2 - 50, 10));
-     boundaries.push(new Boundary(width / 2 - 100, height * (2 / 3), width / 2 - 100, 10));
+    boundaries.push(new Boundary(width / 2, height / 2 + 100, 100, 10));
+    boundaries.push(new Boundary(3 * width / 4, height - 50, width / 2 - 50, 10));
+    boundaries.push(new Boundary(width / 2 - 100, height * (2 / 3), width / 2 - 100, 10));
 }
+
 
 function draw() {
 
     background(0);
 
-  // We must always step through time!
-     var timeStep = 1.0 / 30;
-     world.Step(timeStep, 10, 10);
+    // We must always step through time!
+    var timeStep = 1.0 / 30;
+    world.Step(timeStep, 10, 10);
 
 
-     box.display();
-    //  translate(box.pos.x, box.pos.y);
-
-     spring.update(mouseX,mouseY);
-     spring.display();
-
-     //limb.display();
+    /* CREATE CAMERA SANBOX */
+    push();
+    worldPos = findCenter(box);
+    translate(-worldPos.x, -worldPos.y);
 
 
-
-     // boundary sandbox
-
+    box.display();
 
 
-     for (var i = 0; i < boundaries.length; i++) {
-       boundaries[i].display();
-       boundaries[i].move();
-       boundaries[i].reset();
-     }
+    spring.update(mouseX, mouseY);
+    spring.display();
+
+    //limb.display();
+
+
+
+    // boundary sandbox
+
+
+
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].display();
+        boundaries[i].move();
+        boundaries[i].reset();
+    }
+
+
+    /* END CAMERA SANBOX */
+    pop();
 
 
 }
 
 function mouseReleased() {
-     spring.destroy();
+    spring.destroy();
 }
 
 function mousePressed() {
-     if (box.contains(mouseX, mouseY)) {
-          spring.bind(mouseX, mouseY, box);
-     }
+    if (box.contains(mouseX, mouseY)) {
+        spring.bind(mouseX, mouseY, box);
+    }
 }
